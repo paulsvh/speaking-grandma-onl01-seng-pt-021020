e24b33d85491b70b30a682eddfa0464cd29c1fbe
Paul von Hagen <pvonhagen26@gmail.com>
2020-02-11 04:13:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(speak)
     "I LOVE YOU TOO PUMPKIN!"
   elsif speak != speak.upcase
     "HUH?! SPEAK UP, SONNY!"
-  elsif speak == speak.upcase
+  else
     "NO, NOT SINCE 1938!"
   end
 end
